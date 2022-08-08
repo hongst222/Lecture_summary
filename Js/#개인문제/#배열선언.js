9c3ed870c93098b428112d02e82dbf0afe9d3286
hongst222 <tmdxordusdo@gmail.com>
2022-08-08 18:25:47 +0900

    2022.08.08 수업종료

--- Js/#개인문제/#배열선언.js
+++ Js/#개인문제/#배열선언.js
@@ -41,12 +41,33 @@ console.groupEnd();
 // 입력 받은 배열의 순번과 값을 모두 출력하시오.
 let value4 =[1, 2, 3, 4, 5, 6, 7];
 
-function solution4(inputValue){
+/*function solution4(inputValue){
     let v4v =[];
     value4.forEach((e,i)=>{
         v4v.push("index: "+i+" element: "+e);
     })    
     return v4v;
     
+}*/
+function solution4(inputValue){
+    let output = '';
+    inputValue.forEach((e,i) => {
+        output += `[${i}] : ${e}\n`;
+    });
+    return output;
 }
 console.log(solution4(value4));
+
+// 문제5.
+// 입력 받은 배열의 마지막 값을 제거후 반환하시오.
+let value5 = ["a", "b", "c", "d", 1, 2, 3];
+
+function solution5(inputValue){
+    v = inputValue[inputValue.length -1];
+    inputValue.pop();
+
+    return inputValue;
+}
+console.group("5번문제 실행결과");
+    console.log(solution5(value5));
+console.groupEnd();
